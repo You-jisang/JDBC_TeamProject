31e04a1197a693bf5635493ee258ef1eb096d33a
You-jisang <dbwltkd1019@naver.com>
2024-10-27 16:57:10 +0900

    Initial commit: Setup JDBC project

---
+++ src/main/java/org/example/JDBCConnection.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.*;
+
+
+public class JDBCConnection {
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
